98f7b3347b2696483dcfcb3cdf299ef32a023be9
Joey Feng <jfeng702@gmail.com>
2016-07-14 12:32:23 -0700

    chapter 1

---
+++ exercises.rb
@@ -0,0 +1,36 @@
+#Basics
+# 1.
+"Joey "+"Feng"
+#2.1
+1234/1000
+#2.2
+1234/1000%10
+#2.3
+1234/10%100%10
+#2.4
+1234%1000%100%10
+#3
+a = {:movie_a => 1975, :movie_b => 2004, :movie_c => 2013, :movie_d => 2001, :movie_e => 1981}
+puts a[:movie_a]
+puts a[:movie_b]
+puts a[:movie_c]
+puts a[:movie_d]
+puts a[:movie_e]
+#4
+b = [1975,2004,2013,2001,1981]
+puts b[0]
+puts b[1]
+puts b[2]
+puts b[3]
+puts b[4]
+#5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+#6
+puts 1.1*1.1
+puts 2.2*2.2
+puts 3.3*3.3
+#7
+there is an opening bracket in the program without a closing bracket following it.
